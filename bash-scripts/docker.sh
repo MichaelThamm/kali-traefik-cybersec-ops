a29dfebbff0080805b4a559a58a689fddd6c00f2
Michael Thamm <mikethamm44@gmail.com>
2024-01-31 23:27:38 -0500

    Update scripts

---
+++ bash-scripts/docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker network create web
+docker compose up -d
--- bash-scripts/kali.sh
+++ bash-scripts/kali.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# docker compose exec kali /bin/bash -c ./usr/local/services/kali/cybersec-check.sh
+# All scripts are executed within the kali container since it is acting as our testing black box
 
-docker compose exec kali /bin/bash -c ./usr/local/services/kali/john-ripper.sh
+# Website handshake, list cipher suites, 
+docker compose exec kali /bin/bash -c ./usr/local/services/kali/cybersec-check.sh
+# Brute force the bcrypt hash of the basic auth in front of the webserver with john the ripper
+# docker compose exec kali /bin/bash -c ./usr/local/services/kali/john-ripper.sh
